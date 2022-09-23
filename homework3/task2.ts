f68ee95fb469543a893ca51755575021dad47c87
yury-moroz <yurymoroz89@gmail.com>
2022-09-23 22:45:07 +0300

    add homework3

---
+++ homework3/task2.ts
@@ -0,0 +1,7 @@
+const secMin = 60;
+const minHour = 60;
+const hourDay = 24;
+const dayYear = 365;
+const myAge = 32;
+const myAgeinseconds = myAge * secMin * hourDay * minHour * dayYear;
+console.log(myAgeinseconds);
