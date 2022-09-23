c5aa7c809e370090b93c5eeb722dba1f9c1e6863
yury-moroz <yurymoroz89@gmail.com>
2022-09-23 23:22:42 +0300

    add homework3

---
+++ homework3/task5.ts
@@ -0,0 +1,7 @@
+const a = "доступ";
+const b = "морпех";
+const c = "наледь";
+const d = "попрек";
+const e = "рубило";
+const lengthWords = a.length + b.length + c.length + d.length + e.length;
+console.log(lengthWords);
