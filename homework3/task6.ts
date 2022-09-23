b0372754debf500073f9ec1975913e736e8cc07e
yury-moroz <yurymoroz89@gmail.com>
2022-09-23 23:20:57 +0300

    add homework3

---
+++ homework3/task6.ts
@@ -0,0 +1,6 @@
+const name = "yury";
+const age = 32;
+const c = false;
+console.log("variable " + name + " have type: " + typeof name);
+console.log("variable " + age + " have type: " + typeof age);
+console.log("variable " + c + " have type: " + typeof c);
