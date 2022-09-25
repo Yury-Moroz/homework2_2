23a3d2225bc661c7b0c9353845e550e0c8357175
yury-moroz <yurymoroz89@gmail.com>
2022-09-25 21:55:25 +0300

    add homework3

---
+++ homework3/task12.ts
@@ -0,0 +1,9 @@
+const age = 32;
+if (age >= 18) {
+  console.log("Попей пивка");
+} else if (16 <= age && age < 18) {
+  console.log("Можешь выкурить сигаретку, только маме не говори");
+  console.log("Пей колу");
+} else {
+  console.log("Пей колу");
+}
