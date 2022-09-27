358e180995bd6061501092de88e0e09bc3f5630c
yury-moroz <yurymoroz89@gmail.com>
2022-09-27 15:56:29 +0300

    add homework4

---
+++ homework4/task5.ts
@@ -0,0 +1,5 @@
+const str = "я учу typescript!";
+const result = str.slice(6, 16);
+console.log(result);
+const result2 = str.substring(2, 5);
+console.log(result2);
---
+++ homework4/task6.ts
@@ -0,0 +1,4 @@
+const str = "я учу typescript!";
+console.log(str.slice(0, 1).split(" "));
+console.log(str.substring(2, 5).split(" "));
+console.log(str.slice(6, 17).split(" "));
